d0514cf0e7b43363c788a49d52c3dbc17f8e1a27
Lauro Gomes <lagomes@redhat.com>
2022-07-20 22:26:51 -0300

    send files

--- app/create_cvs_fii.py
+++ app/create_cvs_fii.py
@@ -21,7 +21,7 @@ def analise_fii(NUMBER):
         (df_fii['DY'] >= 0.06 ) & 
         (df_fii['FFOYield'] <= 1) & 
         (df_fii['FFOYield'] >= 0.01 ) &
-        (df_fii['P/VP'] <= 4) & 
+        (df_fii['P/VP'] <= 5) & 
         (df_fii['P/VP'] >= 0.01 ) &
         (df_fii['LIQUIDEZ'] > 5000 )].sort_values(
             by=["DY","P/VP","FFOYield","LIQUIDEZ"],ascending=False)
